22c0d2b18d19eaeeb749fb29971a54a806adb501
diagoxd <95673093+diagoxd@users.noreply.github.com>
2021-12-07 04:03:56 -0300

    Update groupPost.py

--- groupPost.py
+++ groupPost.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-groups = ['251123409000385','843804142348443','900389246658181']
+groups = ['449003915708188','posteadoresdemu','PosteaServerMu','525371681336606','682341498933980','654504961762672','671734949658293','PublicatuservidordeMuOnline1','488942921894012','1408430292755904','1327771757379427','1277855692263838','1197381650437601','1106829526003392','henesysmuonline','posttumuonline','829441777188770','774458819269859'
 
 time.sleep(5)
 
@@ -21,7 +21,63 @@ for i in range(len(groups)):
     pyautogui.typewrite('p')
     time.sleep(2)
     print("Writing post\n")
-    pyautogui.typewrite("Hello there, it's a testing post from messy programmers")
+    pyautogui.typewrite(" "BIENVENIDOS A COMUNIDAD CARBONO
+
+Tenemos 2 servidores de Mu Online  y 1 de Counter Strike 1.6
+
+Servidor Innagurado : Season 2 
+Servidor en progreso :  Season 6 
+Servidor en progreso : Counter Strike 1.6
+🔥◾ Season 2 
+🔥◾ 300 Exp x30% Drop
+🔥◾ 3 cuentas x IP | HWID
+🔥◾ Items Semi (4OPC+L+ADD)
+🔥◾ No Pay To Win
+🔥◾CastleSiege ON
+🔥◾ Eventos originales de MuOnline (Letra H para ver eventos)
+🔥◾ Files + Antihack + Launcher PREMIUM
+🔥◾ Sin helper! Comandos para joyas
+
+Nuestras redes sociales abajo
+
+Web : https://www.mucarbono.com.ar 
+Link Descarga Directo Season 2 : https://www.mediafire.com/file/odhxb5kyik45ep6/Jugar+MU+Carbono+S2.rar/file
+Fanpage : https://www.facebook.com/mucarbono
+Grupo Facebook : https://www.facebook.com/groups/204683464300029/
+Grupo WhatsApp : PEDIRLO
+Grupo Discord : https://discord.gg/UcJBUkVXMc
+Instagram Oficial : https://www.instagram.com/mucarbono/?hl=es-la
+Discord Gamers : https://discord.gg/zavDSPvfK7
+Canal YouTube Tutoriales : https://www.youtube.com/channel/UChziB2LGd7OlRs1hGS2EH8Q
+Discord Private : FacalasitO#7469"		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		")
     time.sleep(4)
 
     pyautogui.keyDown('ctrl')
